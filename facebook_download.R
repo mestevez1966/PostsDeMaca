7c3bb9cdd8d46b710c53ed62b11b19a74b185c3a
Macarena Estevez <36417794+mestevez1966@users.noreply.github.com>
2023-05-11 13:45:41 +0200

    Update facebook_download.R

--- facebook_download.R
+++ facebook_download.R
@@ -126,7 +126,7 @@ page <- "PostsDeMaca"
 
 data <- getPage(page, token
                 ,
-                since = Sys.Date(), until = Sys.time()
+                since = "2023/05/08", until = Sys.time()
 )
 # View(data)
 
